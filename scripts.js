b524515ae47b901b4b8d7f019aabb906579532c1
s3742675 <s3742675@student.rmit.edu.au>
2022-05-21 13:18:57 +1000

    - Re-add condensed versions of test results to Group Profile section - Create project.html page dedicated to Project Idea section of assignment - Link index.html and project.html pages via Project Idea button - Matthew Ablott's answer to Group Processes question

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function goProject() {
+  location.replace("file:///C:/Users/matthew/source/group-project-a3/project.html")
+}
+
+function goHome() {
+  location.replace("file:///C:/Users/matthew/source/group-project-a3/index.html")
+}
