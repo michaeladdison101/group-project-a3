a10ae7c53a591357875c6e2bf98a270ef4b2eb76
s3742675 <s3742675@student.rmit.edu.au>
2022-05-21 18:22:14 +1000

    Fix link between home / project page

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function goProject() {
-  location.replace("file:///C:/Users/matthew/source/group-project-a3/project.html")
+  location.replace("https://s3742675.github.io/group-project-a3/project.html")
 }
 
 function goHome() {
-  location.replace("file:///C:/Users/matthew/source/group-project-a3/index.html")
+  location.replace("https://s3742675.github.io/group-project-a3/")
 }
